77fbebcde103911a0633bc0bfa1e3db4806b64d4
asumin1982 <s_andriano@mail.ru>
2021-03-06 00:24:39 +0300

    Добавил ф-цию add

--- global_area.py
+++ global_area.py
@@ -8,6 +8,7 @@
 В качестве ответа прикрепите файл с полученными данными о среднем росте.
 '''
 #---
+'''
 with open('/home/asumin/Загрузки/dataset_3380_5.txt', 'r', encoding='utf-8') as file: # read file
     arr = [[j for j in i.strip().split() ] for i in file.readlines()]
     # создаем 2d массив [['6', 'Tracey', '155'],['3', 'Lewin', '140'],....]
@@ -32,7 +33,7 @@ for i in range(1, 12):
     if str(i) not in dict_count2.keys():
         print(i,'-')
     else:
-        print(i, (dict_count2[str(i)][1]/dict_count2[str(i)][0]))
+        print(i, (dict_count2[str(i)][1]/dict_count2[str(i)][0]))'''
 #----------------------------------------------------------------------------------------------------------
 '''Реализуйте программу, которая будет эмулировать работу с пространствами имен. 
 Необходимо реализовать поддержку создания пространств имен и добавление в них переменных.
@@ -60,14 +61,17 @@ namesp = {
     }
 }
 
-2. Количество команд считывал через n = int(input()). Затем в цикле while n != 0 считывал команды, сразу деля их на 3 переменные cmd, nmsp, var = input().split() и в зависимости от if cmd == я выполнял ту или иную функцию и передавал в нее остальные 2 переменные nmsp и var.
+2. Количество команд считывал через n = int(input()). Затем в цикле while n != 0 считывал команды, 
+сразу деля их на 3 переменные cmd, nmsp, var = input().split() и в зависимости 
+от if cmd == я выполнял ту или иную функцию и передавал в нее остальные 2 переменные nmsp и var.
 3. У меня было 3 основные функции 
 - def get(namespace, var),
 - def add(namespace,var),
     a['vars'].add(var)
 - def create(namespace, parent)
     a[namespace] = {'parent':parent, 'vars':set()}
-4. Для создания окружения я использовал рекурсивную функцию, которая ищет ключ словаря который будет родительским пространством для нового.
+4. Для создания окружения я использовал рекурсивную функцию, 
+которая ищет ключ словаря который будет родительским пространством для нового.
 
 def finditem(obj, key):
     if key in obj:
@@ -79,36 +83,42 @@ def findvar(obj, namespace, var):
     if namespace in obj:
         if var in obj[namespace]['vars']:
             return namespace
+--------------------------------------------------------------------------
 или 2d массив [global, var, parrent]
               [ ...   ...    ....  ]
 '''
-
-
 #--
-dg = {'global':{'parrent':None, 'vars': set(), 'foo':{'parrent':None, 'vars':set(), 'bar':{'parrent':None, 'vars':set()}}} }
+dg = {'global':
+          {'parrent':None, 'vars': set(),
+           'foo':{'parrent':'global', 'vars':set(),
+                  'bar':{'parrent':'foo', 'vars':set()}}} }
 #--
-namespace_dict = {}
-def add(namespace, var):
+d = {} #  словарь для создания дерева. см. выше ^
+#--      Ф-ция для добавл. namespace, parrent и переменных(var в список)
+def add_var(namespace, var):
+    if namespace  not in d:
+        d[namespace] = {}
+        d[namespace]['parrent'] = None
+        d[namespace]['vars'] = []
+        d[namespace]['vars'].append(var)
+    else: d[namespace]['vars'].append(var)
+    print(d)
+#
+def create_def(namespace, parrent):
     pass
+#
 def get(namespace, var):
     pass
-def create(namespace, parrent):
-    pass
-#--
-#data = command, namespace, var = input('<:').split()
-#--
-<<<<<<< HEAD
-#from collections import defaultdict
-#d = defaultdict(list)
-d= {}
-name = 'one'
-f = 'A'
-for i in range(10):
-    try:
-        d[f] = d.setdefault(f, 0) + i
-    except:
-        break
-print(d)
+#--  основное тело программы ---
+n = 3
+for i in range(n):
+    command, namespace, name = input('<:').split()
+    if command == 'add':
+        add_var(namespace, name)
+    elif command == 'create':
+        create_def(namespace, name)
 
-=======
->>>>>>> 4032175022d7d1841322c26356dc3e9918097926
+#print(d)
+#--
+
+
