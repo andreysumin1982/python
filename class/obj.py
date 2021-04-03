e6556f488059abc0508f54f28a6d4af5682eee88
asumin1982 <s_andriano@mail.ru>
2021-04-03 13:14:22 +0300

    Добавил класс A

--- class/obj.py
+++ class/obj.py
@@ -1,2 +1,21 @@
 '''stepic.org, тесты, задачи на тему ООП'''
-sdfasdfasdfas
+#------------------------------------------------------------------------------
+class A:
+    def __init__(self, val=0):
+        self.val = val
+#
+    def add(self, x):
+        self.val += x
+#
+    def print_val(self):
+        print(self.val)
+#
+a = A()
+b = A(2)
+c = A(4)
+a.add(2)
+b.add(2)
+#
+a.print_val()
+b.print_val()
+c.print_val()
