226b718afab92cb425c84b930adfdac92637e0da
asumin1982 <s_andriano@mail.ru>
2021-04-03 13:40:57 +0300

    Добавил класс Hero

--- class/obj.py
+++ class/obj.py
@@ -19,3 +19,26 @@ b.add(2)
 a.print_val()
 b.print_val()
 c.print_val()
+#--------------------------------------------------------------------------
+class Hero():
+    '''Класс создаем героя для игры'''
+    def __init__(self, name, race, level, force, magic, intelligence):
+        self.name = name
+        self.race = race
+        self.level = level
+        self.force = force
+        self.magic = magic
+        self.intelligence = intelligence
+    '''Добавляем методы'''
+    def name(self):
+        pass
+    def race(self):
+        pass
+    def level(self):
+        pass
+    def force(self):
+        pass
+    def magic(self):
+        pass
+    def intelligence(self):
+        pass
