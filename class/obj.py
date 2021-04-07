64718a74b5555431c8a25750e2f2461350049249
asumin1982 <s_andriano@mail.ru>
2021-04-07 16:26:20 +0300

    Наследование

--- class/obj.py
+++ class/obj.py
@@ -128,8 +128,55 @@ def run():
     return m
 print(run())
 '''---------------------------'''
+#---------------------- Наследование---------------------------
+class Base:
+    def __init__(self):
+        self.val = 0
+
+    def add_one(self):
+        self.val += 1
+        print(self.val)
+
+    def add_many(self, x):
+        for i in range(x):
+            print('add_one_Base')
+            self.add_one()
+
+class Derived(Base):
+    def add_one(self):
+        print('add_one_Dereved')
+        self.val += 10
+        print(self.val)
 
 
+#a = Derived()
+#a.add_one()
+
+b = Derived()
+b.add_many(3)
+
+#print(a.val)
+print(b.val)
+#--     --------------------------------------------
+class A:
+   def foo(self):
+      print("A")
+
+class B(A):
+   pass
+
+class C(A):
+   def foo(self):
+      print("C")
+
+class D:
+   def foo(self):
+      print("D")
+
+class E(B, C, D):
+   pass
+
+E().foo()
 
 
 
