37c82a80255d664e0d598edf5730c23ba17979bd
asumin1982 <s_andriano@mail.ru>
2021-05-11 16:35:35 +0300

    Доделать рекурсию rec

--- exception/except.py
+++ exception/except.py
@@ -15,8 +15,8 @@ except ArithmeticError:
 except AssertionError:
     print("AssertionError")
 #-------------------------------------------
-#PATH = '/home/asumin/Документы/Программирование Python/stepik.org/Основы и применение/Исключения/test'
-PATH = '/home/asumin/Документы/Программирование Python/Stepic.org/Основы и применение/Исключения/test'
+PATH = '/home/asumin/Документы/Программирование Python/stepik.org/Основы и применение/Исключения/test'
+#PATH = '/home/asumin/Документы/Программирование Python/Stepic.org/Основы и применение/Исключения/test'
 #
 def read_file(p):
     with open(p) as file:
@@ -30,7 +30,7 @@ def add_dict(p):
         if len(p[i]) == 1:
             dict_class[p[i][0]] = 'None'  # Убираем пробелы, доводим до состояния {'..':'..'}
         else:
-            dict_class[p[i][0].strip()] = ''.join(p[i][1:]).strip() # Убираем пробелы, доводим до состояния {'..':'..'}
+            dict_class[p[i][0].strip()] = ''.join(p[i][1:]).strip().split() # Убираем пробелы, доводим до состояния {'..':'..'}
     print(dict_class)
 #
 add_dict(r)
@@ -39,10 +39,44 @@ k = int(r[0][0])+2 # для среза
 arr = [''.join(i) for i in r[k:]] # получаем список из запросов.
 print(arr)
 #
-def request(p):
+def rec(parr): # Создаем рекурсию
+    #if dict_class[parr] == 'None':
+    #    return
+    if parr in arr:
+        return parr
+    for i in dict_class[parr]:
+        if rec(i):
+            return parr
+
+
+def request(p): # бежим по списку запросов [arr]
     for i in p:
-        if i in dict_class:
-
-        #print(i,'**')
-
+        print(rec(i))
 request(arr)
+#--------------- Реализация для stepic.org-----------------
+'''def recur(arr_parr):
+    if len(arr_parr) == 0:
+        return
+    k = arr_parr[0]
+    if k in di[k]:
+        return k
+    else:
+        recur(arr_parr[1:])
+
+#
+di = {}
+s = set()
+for i in [input('>').strip().split(':') for j in range(int(input('n1: ')))]:
+    if len(i) == 1:
+        di[i[0].strip()] = 'None'
+    else:
+        di[i[0].strip()] = ''.join(i[1:]).strip().split()
+print(di)
+#
+arr_req = []
+for i in [input('<').strip().split() for j in range(int(input('n2: ')))]:
+    arr_req.append(''.join(i))
+print(arr_req)
+#
+print(recur(arr_req))
+#'''
