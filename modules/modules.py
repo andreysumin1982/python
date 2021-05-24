18d7203c6e94de21ce06a218f0c04cdc47a06030
asumin1982 <s_andriano@mail.ru>
2021-05-24 16:43:44 +0300

    Задача с модулем datetime

--- modules/modules.py
+++ modules/modules.py
@@ -3,3 +3,19 @@ import module1
 import module2
 
 print(3)
+#-------------------------------------------
+import datetime
+from datetime import timedelta
+#
+PATH = '/home/asumin/Документы/Программирование Python/stepik.org/Основы и применение/Модули/test'
+#
+def read_file(path_file):
+    with open(path_file) as file:
+        return [i.strip() for i in file.readlines()]
+print(read_file(PATH))
+
+def run():
+    for i in read_file(PATH)[0].split(' '):
+        print(i)
+
+run()
