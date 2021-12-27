b0f35b27946479fab8b1412ee98cc8322e7ad75e
asumin1982 <s_andriano@mail.ru>
2021-12-27 22:05:26 +0300

    Небольшие изменения

--- weather-app/weather_project/weatherParser/Classes/sql_request.py
+++ weather-app/weather_project/weatherParser/Classes/sql_request.py
@@ -1,21 +1,24 @@
-#
+#-------------------------------------------------
 def showTable(tableName):
     '''Смотрим содержимое таблицы'''
     return f'select * from {tableName}'
 #
+def showID(tablename):
+    return f'SELECT max(id) from {tablename}'
+#-------------------------------------------------
 '''Хранимые процедуры SQL'''
 #
 def insertDataCity(cityName):
     '''Заносим полученные данные в таблицу city'''
     return f"insert into city (name) values ('{cityName}')"
 #
-def insertDataOsadki(description, id_city, id_image):
-    '''Заносим полученные данные в таблицу osadki'''
-    return f"insert into osadki (description, id_city, id_image) values ('{description}', '{id_city}', '{id_image}')"
-#
 def insertDataImage(name, expansion, data):
     '''Заносим полученные данные в таблицу city'''
     return f"insert into image (name, expansion, data) values ('{name}', '{expansion}', '{data}')"
+#
+def insertDataOsadki(description, id_city, id_image):
+    '''Заносим полученные данные в таблицу osadki'''
+    return f"insert into osadki (description, id_city, id_image) values ('{description}', {id_city}, {id_image})"
 #
 def insertDataSummary(id_city, temperature, feels_like, wind, humidity):
     '''Заносим полученные данные в таблицу city'''
