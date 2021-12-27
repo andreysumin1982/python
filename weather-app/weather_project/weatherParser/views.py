236731ac36b42809febbc2368ad0ebec2ad7c985
asumin1982 <s_andriano@mail.ru>
2021-12-27 16:43:22 +0300

    ф-ция index просматривает содержимое таблив в цикле

--- weather-app/weather_project/weatherParser/views.py
+++ weather-app/weather_project/weatherParser/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from .Classes import parsing_weather # Импортируем parsing_weather из Classes
 from .Classes import connect_to_db
+from .Classes import sql_request
 #--
 import requests
 from datetime import datetime
@@ -11,9 +12,14 @@ from django.http import JsonResponse
 def index(request):
     '''Парсинг погоды'''
     if (request.method == 'GET'):
-        context = parsing_weather.getData() # Метод getData() из модуля parsing_weather
-        db = connect_to_db.connectDB()
-        print(db)
+        tableName = ['city', 'osadki', 'image', 'summary'] # Список таблиц в базе
+        #
+        for name in tableName: # Бежим по таблицам
+            context = parsing_weather.getData() # Метод getData() из модуля parsing_weather
+            db = connect_to_db.connectDB()
+            data = db.execute(sql_request.showTable(name))
+            print(data.fetchall())
+        db.close()
 
         return render(request, 'weatherParser/weatherParser.html', context) # Вызвращает html
     elif (request.method == 'POST'):
