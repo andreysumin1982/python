c1bbfc18dd18b24af4abfba771b84dad4768c0eb
asumin1982 <s_andriano@mail.ru>
2021-12-27 16:41:32 +0300

    Добавлены хранимые процедуры sql

--- weather-app/weather_project/weatherParser/Classes/sql_request.py
+++ weather-app/weather_project/weatherParser/Classes/sql_request.py
@@ -1,12 +1,23 @@
+#
 def showTable(tableName):
     '''Смотрим содержимое таблицы'''
     return f'select * from {tableName}'
 #
-def insertData(cityName, imageName, expansion, description):
-    '''Заносим полученные данные в таблицы'''
-    return f"insert into city (name) values ('{cityName}')" \
-           f"insert into image (name, expansion) values ('{imageName}', '{expansion}')" \
-           f"insert into osadki (description) values ('{description}')"
-    #f'insert into city (name) values ({name})'
-
-    pass
+'''Хранимые процедуры SQL'''
+#
+def insertDataCity(cityName):
+    '''Заносим полученные данные в таблицу city'''
+    return f"insert into city (name) values ('{cityName}')"
+#
+def insertDataOsadki(description, id_city, id_image):
+    '''Заносим полученные данные в таблицу osadki'''
+    return f"insert into osadki (description, id_city, id_image) values ('{description}', '{id_city}', '{id_image}')"
+#
+def insertDataImage(name, expansion, data):
+    '''Заносим полученные данные в таблицу city'''
+    return f"insert into image (name, expansion, data) values ('{name}', '{expansion}', '{data}')"
+#
+def insertDataSummary(id_city, temperature, feels_like, wind, humidity):
+    '''Заносим полученные данные в таблицу city'''
+    return f"insert into summary (id_city, temperature, feels_like, wind, humidity) values ('{id_city}', '{temperature}', '{feels_like}', '{wind}', '{humidity}')"
+#
