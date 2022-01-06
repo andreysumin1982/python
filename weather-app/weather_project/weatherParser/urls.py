b98d1f8509c71c3fbea8143c0858c8f07de78472
asumin1982 <s_andriano@mail.ru>
2022-01-07 00:37:45 +0300

    Добавлен favicon.ico

--- weather-app/weather_project/weatherParser/urls.py
+++ weather-app/weather_project/weatherParser/urls.py
@@ -1,11 +1,14 @@
 from django.views.generic import RedirectView
 from django.urls import path
+from django.conf.urls import url
 # Импортируем views из папки приложения weatherParser
 from django.conf.urls.static import static
 from .import views
 #
 urlpatterns = [
-    path('', views.index),# метод index в файле weatherParser/views
-    path('add/', views.addData),# метод addDate в файле weatherParser/views
-    path('del/', views.delTable),# метод delTable в файле weatherParser/views
+    path('', views.index, name='index'),# метод index в файле weatherParser/views
+    path('json/', views.getJson, name='json'),# метод getJson в файле weatherParser/views
+    path('add/', views.addData, name='add'),# метод addDate в файле weatherParser/views
+    path('del/', views.delTable, name='del'),# метод delTable в файле weatherParser/views
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)), # Нужно для иконки во вкладке
 ]
