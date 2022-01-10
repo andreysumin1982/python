66f6b3ae9d7ed4173bd6e93e836c32db6694f1a7
asumin1982 <s_andriano@mail.ru>
2022-01-10 10:54:14 +0300

    Установка corsheaders, j, обновление django

--- weather-app/weather_project/weatherParser/urls.py
+++ weather-app/weather_project/weatherParser/urls.py
@@ -1,6 +1,6 @@
 from django.views.generic import RedirectView
 from django.urls import path
-from django.conf.urls import url
+#from django.conf.urls import url
 # Импортируем views из папки приложения weatherParser
 from django.conf.urls.static import static
 from .import views
@@ -11,5 +11,6 @@ urlpatterns = [
     path('summary/', views.showSummary, name='showSummary'),  # метод showSummary в файле weatherParser/views
     path('add/', views.addData, name='add'),# метод addDate в файле weatherParser/views
     path('del/', views.delTable, name='del'),# метод delTable в файле weatherParser/views
-    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)), # Нужно для иконки во вкладке
+    #url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)) # Нужно для иконки во вкладке
+    path('favicon.ico', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)) # Нужно для иконки во вкладке
 ]
