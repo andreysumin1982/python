bb53ab33701ce0c822f87045669cfe832aa625bc
asumin1982 <s_andriano@mail.ru>
2022-01-30 22:41:46 +0300

    Протестирована ф-ция findNeighbors

--- game of life/test.py
+++ game of life/test.py
@@ -1,3 +1,4 @@
+import copy
 import random
 import time
 #
@@ -6,24 +7,26 @@ current_gen = [[random.randint(0,1) for j in range(n)] for i in range(n)]
 #
 next_gen=[[0 for j in range(n)] for j in range(n)]
 #
-def findNeighbors(current_gen, x,y):
+def findNeighbors(current_gen,x,y):
     # Ф-ция ищет соседей
     count = 0
-    for i in range(y-1, y+2): # цикл от 0 до 3(не включая 3)
-        for j in range(x-1, x+2): # цикл от 0 до 3(не включая 3)
-            if current_gen[i][j]:
+    for iY in range(y-1, y+2): # цикл от 0 до 3(не включая 3)
+        for jX in range(x-1, x+2): # цикл от 0 до 3(не включая 3)
+            print(current_gen[iY][jX], end=', ')
+            if current_gen[iY][jX] == 1:
                 count +=1
-        if current_gen[y][x]:
-            count -=1
-            if count ==2 or count == 3:
-                return 1
-            else:
-                return 0
+    if current_gen[y][x] == 1:
+        count -=1
+        if count == 2 or count == 3:
+            return 1
         else:
-            if count ==3:
-                return 1
-            else:
-                return 0
+            return 0
+    else:
+        if count == 3:
+            print('Новая жизнь: ', 1)
+            return 1
+        else:
+            return 0
 # Выводим матрицу
 for k in current_gen:
     print(k)
@@ -32,8 +35,24 @@ print(20*'-')
 #   0 1 0
 #   0 0 0
 # Циклом идем от 1 до предпоследнего элемента
-for i in range(1, len(current_gen)-1):
-    print('i: ',current_gen[i])
-    for j in range(1, len(current_gen)-1):
-        print('j: ',current_gen[i][j])
-        print('Соседи: ', findNeighbors(current_gen, i,j))
+count = 0
+while True:
+    if count < 3:
+        for i in range(1, len(current_gen)-1):
+            print('i: ',current_gen[i])
+            for j in range(1, len(current_gen)-1):
+                fn = findNeighbors(current_gen, i,j)
+                print('j: ',current_gen[i][j])
+                print('Соседи: ', fn)
+                if fn == 1:
+                    next_gen[j][i] = 1
+                next_gen[j][i] = 0
+        for l in next_gen:
+            print(l)
+        current_gen = copy.deepcopy(next_gen)
+        count +=1
+        time.sleep(1)
+    else:
+        break
+
+#---
