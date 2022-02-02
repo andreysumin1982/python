85c3b4a5c926960e58cb591db22d7e2f284c07f1
asumin1982 <s_andriano@mail.ru>
2022-02-02 16:58:51 +0300

    Реализована игра Life с использованием ООП

--- game of life/mainObj.py
+++ game of life/mainObj.py
@@ -1,35 +1,64 @@
 import copy
 import pygame as p
 import random
+
+
 #
-class Life:
-    def __init__(self, matrix):
-        self.matrix = matrix
-    def findNeighbors(self):
-        pass
-    def nextGenerations(self):
-        pass
-    def getMatrix(self):
-        return self.matrix
-#
-class Game():
+class Life():
     def __init__(self):
-        # init запускает pygame.
+        pass
+
+    def findNeighbors(self, current_gen, x, y):
+        # Ф-ция ищет соседей
+        # в пустой клетке, рядом с которой ровно 3 живые клетки, зарождается жизнь
+        # если у живой клетки есть 2 или 3 живых соседки, то эта клетка продолжает жить
+        # если у живой клетки меньше 2 или больше 3 живых соседки, клетка умирает
+        self.current_gen = current_gen
+        self.x = x
+        self.y = y
+        count = 0
+        for iY in range(y - 1, y + 2):  # цикл от 0 до 3(не включая 3)
+            for jX in range(x - 1, x + 2):  # цикл от 0 до 3(не включая 3)
+                # print(current_gen[iY][jX], end=', ')
+                if current_gen[iY][jX] == 1:
+                    count += 1
+        if current_gen[y][x] == 1:  # Если проверяемый элем. == 1
+            count -= 1
+            if count == 2 or count == 3:
+                return 1
+            else:
+                return 0
+        else:
+            if count == 3:
+                # print('Новая жизнь: ', 1)
+                return 1
+            else:
+                return 0
+#
+class Game(Life):
+    '''Класс Game:
+    * Наследует класс Life
+    * Инициализирует импортируемый модуль pygame
+    * Создает игровое окно и отрисовывает логику класса Life'''
+    def __init__(self):
+        # init, запускает pygame.
         p.init()
         self.setColor()
 
     def setWindow(self, width, height, fps=60):
-        # окно программы, задаем его размеры в настройках.
+        # Игровое окно, задаем его размеры и fps.
         self.width = width
         self.height = height
         self.fps = fps
+        # Игровое окно
         self.screen = p.display.set_mode((width, height))
         self.clock = p.time.Clock().tick(fps)
 
-    def setCaption(self, text):
-        # заголовок окна
-        self.text = 'Live'
-        p.display.set_caption(str(text))
+    def Generations(self):
+         # Создаем двумерный массив и заполняем его (0,1) в случ. порядке
+         self.current_gen = [[random.randint(0, 1) for j in range(self.width // 20)] for i in range(self.height // 20)]
+         # Создаем двумерный массив для след. поколения, и заполняем  (0)
+         self.next_gen = [[0 for j in range(self.width // 20)] for j in range(self.height // 20)]
 
     def setColor(self):
         # Цвета
@@ -40,6 +69,7 @@ class Game():
         self.BLUE = (0, 0, 255)
 
     def drowGrid(self, gridScreen, gridColor):
+        # Сетка,
         self.gridScreen = gridScreen
         self.gridColor = gridColor
         #
@@ -49,31 +79,54 @@ class Game():
         for j in range(0, self.width // 20):
             p.draw.line(self.screen, gridColor, (j * 20, 0), (j * 20, self.height))
 
-    def drowRectangle(self, rectColor, rectScreen):
+    def drowRectangle(self, rectColor, rectScreen, x,y):
+        # Прямоугольник
         self.rectColor = rectColor
         self.rectScreen = rectScreen
+        self.x = x
+        self.y = y
+        p.draw.rect(rectScreen, (rectColor), (x * 20, y * 20, 20, 20))
 
     def run(self):
-        # Запускаем игру
-        self.setWindow(1600,800,60)
-        print(self.screen)
-        self.setCaption('Live')
-        print(self.screen.fill(self.WHITE))
+        # Бесконечный цикл
         while True:
             for event in p.event.get():  # смотрим события
                 if event.type == p.QUIT:  # p.QUIT код: 256 - exit
-                    exit(0)
+                    exit(0) # Выход
                 else:
+                    # Закрашиваем окно
                     self.screen.fill(self.WHITE)
+                    # Рисуем сетку под размер окна
                     self.drowGrid(self.screen, self.BLACK)
-                    #
+                    # Бежим по массиву
+                    for x in range(1, (self.width // 20) - 1):
+                        for y in range(1, (self.height // 20) - 1):
+                            # Рисуем только живые клетки
+                            if self.current_gen[y][x] == 1:
+                                self.drowRectangle(self.GREEN, self.screen, x, y)
+                            # Проверяем каждую клетку на наличие соседей и переносим ее в массив(следующее поколение)
+                            self.next_gen[y][x] = self.findNeighbors(self.current_gen, x, y)
+                    # После проверки всех клеток, копируем полученный массив (следующего поколения), и присваиваем переменную current_gen.
+                    self.current_gen = copy.deepcopy(self.next_gen)
+                    print(id(self.current_gen))
+                    # После отрисовки, обновляем экран
                     p.display.update()
 
 #
 if __name__ == '__main__':
+    # Экземпляр класса Life
+    life = Life()
+    # Экземпляр класса Game
     game = Game()
+    # Создаем игровое окно
+    game.setWindow(1900,800,60) #
+    # Создаем текущее поколение
+    game.Generations()
+    # Запускаем игру
     game.run()
     #
+
+
     # # создаем игру и окно
     # p.init()  # init запускает pygame.
     # screen = p.display.set_mode((800, 800))  # окно программы, задаем его размеры в настройках.
