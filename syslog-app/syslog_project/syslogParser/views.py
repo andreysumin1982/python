736b54ad3e6d852bbfd1b320b5edd449c4ce5d2c
asumin1982 <s_andriano@mail.ru>
2022-02-06 20:39:51 +0300

    Рендеринг

--- syslog-app/syslog_project/syslogParser/views.py
+++ syslog-app/syslog_project/syslogParser/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     if (request.method == 'GET'):
         print('ok')
-    return HttpResponse('ok')
+    return render(request, 'syslogParser/index.html')
