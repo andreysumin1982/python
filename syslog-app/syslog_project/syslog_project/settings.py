c445800b71b57e71be62a082b620f971b95006a2
asumin1982 <s_andriano@mail.ru>
2022-02-14 16:43:56 +0300

    Подкл. всем

--- syslog-app/syslog_project/syslog_project/settings.py
+++ syslog-app/syslog_project/syslog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)p2(gkdxxgqf3v7c=(vpf3jkg98+ns79b1o9h@-+3x9dx0x+92
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
