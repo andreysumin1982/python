6f17775af04cafbc7f010b5bd6c2103f4e89550b
asumin1982 <s_andriano@mail.ru>
2022-06-07 16:37:41 +0300

    нет изменений

--- postgres-app/main.py
+++ postgres-app/main.py
@@ -5,7 +5,11 @@ def run():
     db = Postgres('localhost', 'cities', 'test', 'test')
     # Выполнить запрос
     db.executeRequest('SELECT version();')
-    db.executeRequest('SELECT * FROM weather;')
+    db.executeRequest('SELECT * FROM name_of_the_cities;')
+    # Смотрим № транзакций:
+    # Строка создана прудыдущей транзакцией, а xmax = 0 означает, что эта версия актуальна.
+    db.executeRequest('SELECT *, xmin, xmax FROM name_of_the_cities')
+
 
 #
 if __name__ == '__main__':
