ffa51444e16b118802c919c19d7ac376874b0345
asumin1982 <s_andriano@mail.ru>
2022-07-05 16:34:44 +0300

    Доделаны тестовые API

--- drf-app/drf_project/drf_api/urls.py
+++ drf-app/drf_project/drf_api/urls.py
@@ -3,6 +3,9 @@ from django.urls import path # include уже не нужно использ.
 from . import views # обязательно импортируем файл views
 urlpatterns = [
     path('', views.index, name='index'), # здесь уже использ. файл views и метод index в этом файле
-    path('api/1/', views.testApi, name='testApi'),
+    path('api/v1/test/', views.testApi, name='testApi'),
+    path('api/v2/test2/', views.testApi2, name='testApi2'),
+
+    #-Иконка-#
     path('favicon.ico', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)) # Нужно для иконки во вкладке
 ]
--- drf-app/drf_project/drf_api/views.py
+++ drf-app/drf_project/drf_api/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
+from .serializers import encodeTest
+from .serializers import encodeTest2
 # Create your views here.
 
 #
@@ -10,6 +12,9 @@ def index(request):
 #
 def testApi(request):
     if (request.method == 'GET'):
-        class testAPIView(generics.ListAPIView):
-            queryset = render(request, 'drf_api/index.html')
-            serializer_class = testSerializer;
+        return HttpResponse(encodeTest(), content_type='application/json')
+#
+def testApi2(request):
+    if (request.method == 'GET'):
+        arrlist = [random.randint(1,1000) for j in range(random.randrange(1000))]
+        return HttpResponse(encodeTest2(random.randrange(100), arrlist), content_type='application/json')
